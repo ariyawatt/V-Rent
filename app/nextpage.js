b62877b26e9609528bcd924bb9eb6c029149d6e8
H2oZN <Phonlatues@gmail.com>
2025-08-14 22:29:47 +0700

    first commit

---
+++ app/nextpage.js
@@ -0,0 +1,7 @@
+export default function NextPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">This is the Next Page</h1>
+    </div>
+  );
+}
