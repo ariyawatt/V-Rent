a445b2865267dffc2fa7136aa07b0b67c12a2527
H2oZN <Phonlatues@gmail.com>
2025-09-17 13:57:05 +0700

    fix lost image and remove company temp

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "v-rent-app-916879005749.asia-southeast1.run.app",
         pathname: "/**",
-      }
+      },
       // ถ้ามีโดเมนรูปอื่น ๆ ใส่เพิ่มที่นี่
     ],
+    unoptimized: true,
   },
 };
 
